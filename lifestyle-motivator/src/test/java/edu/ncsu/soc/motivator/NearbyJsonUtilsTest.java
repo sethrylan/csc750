faf1188d77a8d1452ae1352a304c911d6fd45a3b
sethrylan <seth.rylan@gmail.com>
2013-04-04 22:29:42 -0400

    Added unit test.

---
+++ lifestyle-motivator/src/test/java/edu/ncsu/soc/motivator/NearbyJsonUtilsTest.java
@@ -0,0 +1,39 @@
+package edu.ncsu.soc.motivator;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import edu.ncsu.soc.motivator.domain.Nearby;
+
+@RunWith(Parameterized.class)
+public class NearbyJsonUtilsTest extends JsonUtilsTest {
+
+    private static final String FILE_REGEX = ".*nearby.*.json";
+    
+    private String filename;
+    private String json;
+
+    public NearbyJsonUtilsTest(String filename, String json) {
+        this.filename = filename;
+        this.json = json;
+    }
+
+    @Parameters
+    public static Collection<Object[]> parameters() {
+        return testFiles(FILE_REGEX);
+    }
+    
+    @Test
+    public void testJson() {
+        Nearby nearby = JsonUtils.createFromJson(Nearby.class, this.json);
+        assertNotNull(nearby);        
+    }
+    
+}
